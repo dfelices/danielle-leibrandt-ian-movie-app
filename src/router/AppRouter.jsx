889e24a1cda01a944d1107e81bfb9ef21fcfc07e
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-01-30 14:48:35 -0800

    added add/remove favorites functionality

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,18 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageHome from "../pages/PageHome";
 // import PageAbout from '../pages/PageAbout';
 // import PageFavorites from '../pages/PageFavorites';
+import { GlobalProvider } from "../context/GlobalContext";
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      {/* <Header />
+      <GlobalProvider>
+        {/* <Header />
             <Nav /> */}
-      <Routes>
-        <Route path="/" element={<PageHome />} />
-        {/* <Route path="/favorites" element={<PageFavorites />} />
+        <Routes>
+          <Route path="/" element={<PageHome />} />
+          {/* <Route path="/favorites" element={<PageFavorites />} />
                 <Route path="/about" element={<PageAbout />} /> */}
-      </Routes>
-      {/* <Footer /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </GlobalProvider>
     </BrowserRouter>
   );
 }
