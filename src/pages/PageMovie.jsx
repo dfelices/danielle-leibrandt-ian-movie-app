b0176e80de1661e2ae32c8b0327bed5798497541
HHan0330 <kkoma330@gmail.com>
2025-02-02 20:23:36 -0800

    add CSS style

--- src/pages/PageMovie.jsx
+++ src/pages/PageMovie.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { getMovies, getMovieCast, getTrailer } from "../utilities/api";
 import { URL_IMAGE } from "../globals/globals";
 import { formatRating, formatRuntime, formatReleaseDate } from "../utilities/toolbelt";
+import "../styles/Movies.css";
 
 const PageMovie = () => {
 
@@ -45,66 +46,71 @@ const PageMovie = () => {
 
     return (
         <>
-            <div className="page-header">
-                <h1>{movie.title}</h1>
-            </div>
-            <div className="single-movie-container">
-                <img
-                    className="single-movie-poster"
-                    src={`${URL_IMAGE}w185/${movie.poster_path}`}
-                    alt={`${movie.title} poster`} />
-                <div className="single-movie-info">
-                    <p>{formatReleaseDate(movie.release_date)}</p>
-                    <ul className="single-movie-genre">
-                        {movie.genres && movie.genres.map((genre, index) => (
-                            <li key={index}>{genre.name}</li>
-                        ))}
-                    </ul>
-                    <p>{formatRuntime(movie.runtime)}</p>
-                    <div className="trailer-section">
-                        {trailer ? (
-                            <a
-                                href={`https://www.youtube.com/watch?v=${trailer.key}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="trailer-button"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
-                                    <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
-                                </svg>
-                            </a>
-                        ) : (
-                            <p>No trailer available.</p>
-                        )}
-                        <p>{formatRating(movie.vote_average)}</p>
-                    </div>
-                    <p>{movie.overview}</p>
-                    {/* Casting List */}
-                    <div className="movie-cast">
-                        <h3>Cast</h3>
-                        <div className="cast-list">
-                            {cast.length > 0 ? (
-                                cast.slice(0, 10).map((actor) => (
-                                    <div key={actor.id} className="actor-card">
-                                        <img
-                                            className="actor-image"
-                                            src={
-                                                actor.profile_path
-                                                    ? `${URL_IMAGE}w185/${actor.profile_path}`
-                                                    : "https://via.placeholder.com/185x278.png?text=No+Image"
-                                            }
-                                            alt={actor.name}
-                                        />
-                                        <p>{actor.name}</p>
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No cast information available.</p>
-                            )}
+            <section className="single-movie-wrapper">
+                <div className="single-movie-title">
+                    <h1>{movie.title}</h1>
+                </div>
+                <div className="single-movie-container">
+                    <div className="single-movie-poster">
+                        <img
+                            className="movie-poster"
+                            src={`${URL_IMAGE}w342/${movie.poster_path}`}
+                            alt={`${movie.title} poster`} />
+                        <div className="single-movie-info">
+                            <p>{formatReleaseDate(movie.release_date)}</p>
+                            <ul className="single-movie-genre">
+                                {movie.genres && movie.genres.map((genre, index) => (
+                                    <li key={index}>{genre.name}</li>
+                                ))}
+                            </ul>
+                            <p className="single-movie-runtime">{formatRuntime(movie.runtime)}</p>
+                            <div className="trailer-section">
+                                {trailer ? (
+                                    <a
+                                        href={`https://www.youtube.com/watch?v=${trailer.key}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="trailer-button"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
+                                            <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
+                                        </svg>
+                                    </a>
+                                ) : (
+                                    <p>No trailer available.</p>
+                                )}
+                                <div className="rating-circle">
+                                    <p>{formatRating(movie.vote_average)}</p>
+                                </div>
+                            </div>
+                            <p className="single-movie-overview">{movie.overview}</p>
                         </div>
                     </div>
                 </div>
-            </div>
+
+                {/* Casting List */}
+                <div className="movie-cast-section">
+                    <h3>Cast</h3>
+                    <div className="movie-cast-list">
+                        {cast.length > 0 ? (
+                            cast.slice(0, 10).map((actor) => (
+                                <div key={actor.id} className="actor-card">
+                                    <img
+                                        className="actor-image"
+                                        src={actor.profile_path
+                                            ? `${URL_IMAGE}w92/${actor.profile_path}`
+                                            : "https://via.placeholder.com/185x278.png?text=No+Image"
+                                        }
+                                        alt={actor.name} />
+                                    <p>{actor.name}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No cast information available.</p>
+                        )}
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
