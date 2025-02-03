ce489896a1d17376d3fd1fc9e805948ccf50d4ad
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-02-03 14:25:11 -0800

    added functional menu button to nav bar, adjusted font weight and sizes, added images folder to public folder and added icons to it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,8 +32,14 @@ const Header = () => {
         >
           <span className="hamburger-icon">
             {navOpen? ( 
-              <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ width: '100%', height: '100%' }}><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>
-              ) : (<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ width: '100%', height: '100%' }}><path d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z" fill-rule="nonzero"/></svg>
+              <div className="menu-icon">
+                <img src={`images/iconmonstr-x-mark-lined-64.png`} alt="Magnifier Icon" />
+              </div>
+
+              ) : (
+              <div className="close-icon">
+                <img src={`images/iconmonstr-menu-right-lined-64.png`} alt="Close Icon" />
+              </div>
             )}
           </span>
           {/* <span className="sr-only">Menu</span> */}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,52 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Nav = ({navOpen, setNavOpen}) => {
   function closeNav(e) {
     setNavOpen(false)
   }
 
+  // State to manage visibility based on screen width
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 768);
+
+  // Function to handle window resize
+  const handleResize = () => {
+    setIsWideScreen(window.innerWidth >= 768);
+  };
+  
+  // Effect to add window resize listener
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+  
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <nav className={`main-nav ${navOpen ? 'show' : ''}`} onClick={closeNav}>
-      {navOpen? (
-        <ul>
-          <li>
-            <NavLink to="/favorites">Faves</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/search">Search</NavLink>
-          </li>
-        </ul>
-        ) : null }
+    <nav className={`main-nav ${navOpen || isWideScreen ? 'show' : 'hide'}`} onClick={closeNav}>
+          <ul className={navOpen || isWideScreen ? 'show' : 'hide'}>
+            <div className="border-left">
+              <li>
+                <NavLink to="/favorites">faves</NavLink>
+              </li>
+            </div>
+            <div className="border-left">
+              <li>
+                <NavLink to="/about">about</NavLink>
+              </li>
+            </div>
+            <div className="border-left">
+              <li>
+                <NavLink to="/search">
+                <div className="search-icon">
+                  <img src={`images/iconmonstr-magnifier-lined-64.png`} alt="Magnifier Icon" />
+                </div>
+                </NavLink>
+              </li>
+            </div>
+          </ul>
     </nav>
   );
 };
