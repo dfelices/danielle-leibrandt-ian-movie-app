ced2867fc36f72757c2c8af441b86cbe15af8517
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-02-07 17:18:43 -0800

    edited responsive styling for header + nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,41 +12,46 @@ const Header = () => {
   return (
     <div className="border">
       <header className={navOpen ? "show" : ""}>
-        <p className="logo">
-          <Link to="/">
-            <span className="mobile-logo">m</span>
-            <span className="desktop-logo">moo.v</span>
-          </Link>
-        </p>
-        {/**
-         * HTML for the Hamburger icon modified from HTMl
-         * found at this codepen:
-         * https://codepen.io/RRoberts/pen/ZBYaJr
-         */}
-        <Nav navOpen={navOpen} setNavOpen={setNavOpen} />
-        <button
-          className="btn-main-nav"
-          onClick={() => {
-            setNavOpen(!navOpen)
-          }}
-        >
-          <span className="hamburger-icon">
-            {navOpen? ( 
-              <div className="menu-icon">
-                <img src={`images/iconmonstr-x-mark-lined-64.png`} alt="Magnifier Icon" />
-              </div>
+        <div className="mobile-menu">
+          <p className="logo">
+            <Link to="/">
+              <span className="mobile-logo">m</span>
+              <span className="desktop-logo">moo.v</span>
+            </Link>
+          </p>
+          <button
+            className="btn-main-nav"
+            onClick={() => {
+              setNavOpen(!navOpen)
+            }}
+            >
+            <span className="hamburger-icon">
+              {navOpen? ( 
+                <div className="menu-icon">
+                  <img src={`images/iconmonstr-x-mark-lined-64.png`} alt="Magnifier Icon" />
+                </div>
 
-              ) : (
-              <div className="close-icon">
-                <img src={`images/iconmonstr-menu-right-lined-64.png`} alt="Close Icon" />
-              </div>
-            )}
-          </span>
-          {/* <span className="sr-only">Menu</span> */}
-        </button>
+  ) : (
+    <div className="close-icon">
+                  <img src={`images/iconmonstr-menu-right-lined-64.png`} alt="Close Icon" />
+                </div>
+              )}
+            </span>
+            {/* <span className="sr-only">Menu</span> */}
+          </button>
+        </div>
+        <Nav navOpen={navOpen} setNavOpen={setNavOpen} />
       </header>
     </div>
   );
 };
 
 export default Header;
+
+
+
+{/**
+ * HTML for the Hamburger icon modified from HTMl
+ * found at this codepen:
+ * https://codepen.io/RRoberts/pen/ZBYaJr
+ */}
